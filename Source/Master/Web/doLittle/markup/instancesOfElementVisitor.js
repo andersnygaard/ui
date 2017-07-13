edfc4827adf5d78807b1fd3fae29e952323c53a8
Einar Ingebrigtsen <einar@dolittle.com>
2017-07-13 13:23:12 +0200

    Adding license header

--- Source/Master/Web/doLittle/markup/instancesOfElementVisitor.js
+++ Source/Master/Web/doLittle/markup/instancesOfElementVisitor.js
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 doLittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 // IMPORTANT: This should be discovered by being able to take dependency to "instancesOfElementVisitor"
 // the IOC will resolve it by convention and find implementations of it
 // It will be a combination of a Babel plugin and IOC magic
--- Source/Master/Web/doLittle/markup/visitors/View.js
+++ Source/Master/Web/doLittle/markup/visitors/View.js
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 doLittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 import {ElementVisitor} from "../ElementVisitor";
 
 export class View extends ElementVisitor
