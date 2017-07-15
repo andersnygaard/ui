d95cd9645ffdd838a49561af437c99b1b3542442
Einar Ingebrigtsen <einar@dolittle.com>
2017-07-15 16:23:55 +0200

    Using the ActionsPerformer

--- Specifications/Master/Web/doLittle/markup/for_ObjectModel/given/all_dependencies.js
+++ Specifications/Master/Web/doLittle/markup/for_ObjectModel/given/all_dependencies.js
@@ -1,7 +1,3 @@
-class Actions 
-{
-}
-
 export default class
 {
     constructor() {
@@ -10,9 +6,16 @@ export default class
         this.elementVisitors = {
             visit: sinon.stub()
         };
-        this.actions = new Actions();
+        this.actions = {my:"actions"};
+        this.results = {my:"results"};
         this.actionsFactory = {
             create: sinon.stub().returns(this.actions)
         };
+        this.actionsPerformer = {
+            perform: sinon.stub()
+        }
+        this.elementVisitorResultsFactory = {
+            create: sinon.stub().returns(this.results)
+        }
     }
 }
---
+++ Specifications/Master/Web/doLittle/markup/for_ObjectModel/given/an_object_model.js
@@ -0,0 +1,15 @@
+import {ObjectModel} from "doLittle/markup/ObjectModel";
+import all_dependenices from "./all_dependencies";
+
+export default class extends all_dependenices
+{
+    constructor() {
+        super();
+        this.objectModel = new ObjectModel(
+            this.document, 
+            this.elementVisitors, 
+            this.actionsFactory, 
+            this.actionsPerformer,
+            this.elementVisitorResultsFactory);
+    }
+}
