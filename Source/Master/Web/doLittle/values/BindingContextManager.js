43cd1c306eb3eb25aa31e64b28d82ca801f212d9
Einar Ingebrigtsen <einar@dolittle.com>
2017-07-15 13:45:00 -0400

    Started looking at the needs

---
+++ Source/Master/Web/doLittle/values/BindingContextManager.js
@@ -0,0 +1,23 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 doLittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import {BindingContext} from "./BindingContext";
+
+ /**
+  * Represents a manager for working with {BindingContext}s
+  */
+export class BindingContextManager {
+
+    hasParent(element) {
+
+    }
+
+    getParent(element) {
+
+    }
+
+    createFor(element) {
+
+    }
+}
