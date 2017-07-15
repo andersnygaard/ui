6d23eba0f194d799591670bc396db6be355d153d
Einar Ingebrigtsen <einar@dolittle.com>
2017-07-15 13:46:13 -0400

    Fleshing out

---
+++ Source/Master/Web/doLittle/views/ViewDefinitionLoadTask.js
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 doLittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import {Task} from "doLittle/tasks/Task";
+
+export class ViewDefinitionLoadTask extends Task
+{
+    constructor(viewPath, viewModelPath) {
+
+    }
+}
