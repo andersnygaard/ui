3ada612949dd59a40e807de360e5d545e8fca1d8
Einar Ingebrigtsen <einar@dolittle.com>
2017-07-15 11:48:48 +0200

    NullViewModel for those views without specific ViewModel

---
+++ Source/Master/Web/doLittle/views/NullViewModel.js
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 doLittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import {ViewModel} from "./ViewModel";
+
+/**
+ * Represents a null implementation of a {ViewModel}
+ */
+export class NullViewModel extends ViewModel
+{
+
+}
