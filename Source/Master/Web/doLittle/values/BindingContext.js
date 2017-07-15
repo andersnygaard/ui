f8b8cb494aab84afae923e151edbc78ac6a13a40
Einar Ingebrigtsen <einar@dolittle.com>
2017-07-15 13:43:23 -0400

    Fleshing out what is needed

--- Source/Master/Web/doLittle/values/BindingContext.js
+++ Source/Master/Web/doLittle/values/BindingContext.js
@@ -3,17 +3,19 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+const _parent = new WeakMap();
 const _children = new WeakMap();
 
 /**
  * Represents the context in which everything is bound relative to
  */
-export class BindingContext
-{
+export class BindingContext {
     /**
      * Initializes a new instance of {BindingContext}
+     * @param {BindingContext} [parent] Parent binding context, if any
      */
-    constructor() {
+    constructor(parent) {
+        _parent.set(this, parent);
         _children.set(this, []);
     }
 
@@ -23,5 +25,23 @@ export class BindingContext
      */
     get children() {
         return _children.get(this);
+    }
+
+    /**
+     * Gets wether or not {BindingContext} has a parent
+     * @returns {Boolean}
+     */
+    get hasParent() {
+        let parent = _parent.get(this);
+        return typeof parent !== "undefined" && parent !== null;
+
+    }
+
+    /**
+     * Gets the parent of the {BindingContext}
+     * @returns {BindingContext}
+     */
+    get parent() {
+        return _parent.get(this);
     }
 }
